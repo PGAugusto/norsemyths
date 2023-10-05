9be0f65d33fd9986fcf4fb36757aedcf50c6a3c0
PGAugusto <115928825+PGAugusto@users.noreply.github.com>
2023-10-05 20:27:15 +0200

    subida documentos Norse Myths

---
+++ javascripts/funciones.js
@@ -0,0 +1,12 @@
+/***************  JAVASCRIPT FUNCTIONS ***************/
+function myFunction() {
+  var x = document.getElementById("myTopnav");
+  if (x.className === "topnav") {
+    x.className += " responsive";
+  } else {
+    x.className = "topnav";
+  }
+}
+
+
+
